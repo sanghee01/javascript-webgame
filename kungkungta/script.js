671466ff0b0a21b8b3cde7156862abf1eebb86e1
sanghee01 <sang6299@naver.com>
2022-03-05 11:31:44 +0900

    쿵쿵따 - 나의풀이

---
+++ kungkungta/script.js
@@ -0,0 +1,39 @@
+const number = Number(prompt("몇 명이 참가하나요?"));
+if (number) {
+  const $input = document.querySelector("input");
+  const $button = document.querySelector("button");
+  const $order = document.querySelector("#order");
+  const $word = document.querySelector("#word");
+  let word;
+  let newWord;
+
+  function onButton() {
+    if (
+      (!word && newWord.length === 3) ||
+      (newWord.length === 3 && word[word.length - 1] === newWord[0])
+    ) {
+      word = newWord;
+      $word.textContent = word;
+      let order = Number($order.textContent);
+      if (order + 1 > number) {
+        $order.textContent = 1;
+      } else {
+        $order.textContent = order + 1;
+      }
+    } else {
+      alert(
+        "세글자, 그리고 입력하신 단어의 첫글자가 제시어의 마지막 글자와 일치하게 입력하세요."
+      );
+    }
+    $input.value = "";
+    $input.focus();
+  }
+
+  function onInput(event) {
+    newWord = event.target.value;
+  }
+
+  $input.addEventListener("input", onInput);
+  $button.addEventListener("click", onButton);
+} else {
+}
